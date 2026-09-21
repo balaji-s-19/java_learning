63f5ae65fa8fd2a0e063a3f3dc7b0e1232fa5910
balaji-s-19 <balayuva2020@gmail.com>
2026-09-21 16:12:36 +0530

    day1

---
+++ controlstructure.java
@@ -0,0 +1,15 @@
+public class controlstructure {
+    public static void main(String[] args) {
+        int x =10;
+        int y =20;
+        System.out.println("x: "+ x);
+        System.out.println("y: "+ y);
+        if(x>y){
+        System.out.println("x is greater than y");
+        }
+        else{
+            System.out.println("y is grater than x");
+        }
+    }
+    
+}
